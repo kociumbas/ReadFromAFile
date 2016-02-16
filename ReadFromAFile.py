ce2cbafdc5ff3fbd5ee31569c014bb17042deaaf
kociumbas <kociumbas@outlook.com>
2016-02-16 01:15:25 +0100

    ReadFromAFile

---
+++ ReadFromAFile.py
@@ -0,0 +1,9 @@
+﻿import csv
+
+fileName = "GuestsList.txt"
+accessMode = "r"
+
+with open(fileName, accessMode) as myCSVFile:
+    dataFromFile = csv.reader(myCSVFile)
+    for row in dataFromFile:
+        print (", ".join(row))  #removes square brackets by joining list items
